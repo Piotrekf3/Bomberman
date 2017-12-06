1322159ddf3b3a8510dc7bc7b333ff5a835f19d7
Piotrekf3 <piotrek.fur@wp.pl>
2017-12-06 12:58:34 +0100

    serwer oczekuje na poloczenie

--- Server/Server/main.cpp
+++ Server/Server/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
+#include <fcntl.h>
+#include <unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 using namespace std;
 
-int main()
-{
-    cout<<"hello\n";
+const char * serverIp="192.168.0.19";
+
+int main(int argc, char **argv) {
+    int sd=socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+
+    sockaddr_in saddr;
+    saddr.sin_family=AF_INET;
+    saddr.sin_port=htons(2500);
+    saddr.sin_addr.s_addr=inet_addr(serverIp);
+
+    bind(sd,(sockaddr*) &saddr,sizeof(saddr));
+    listen(sd,1);
+
+    while(1)
+        if(accept(sd, nullptr, nullptr)>=0)
+            cout<<"poloczenie\n";
+
+    close(sd);
     return 0;
 }
