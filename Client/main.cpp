a4770741ea007247efae490230c5e25723508630
Piotrekf3 <piotrek.fur@wp.pl>
2017-12-09 13:32:32 +0100

    client wyswietlanie planszy

--- Client/main.cpp
+++ Client/main.cpp
@@ -8,9 +8,13 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 const char * ip = "127.0.0.1";
+const int mapWidth = 10;
+const int mapHeight = 10;
+int gameMap[mapWidth][mapHeight];
 
 ssize_t readData(int fd, char * buffer, ssize_t buffsize) {
     auto ret = read(fd, buffer, buffsize);
@@ -24,13 +28,50 @@ void writeData(int fd, char * buffer, ssize_t count) {
     if(ret!=count) error(0, errno, "wrote less than requested to descriptor %d (%ld/%ld)", fd, count, ret);
 }
 
+void sfmlWindow()
+{
+	sf::RenderWindow window(sf::VideoMode(500,500),"Bomberman");
+	sf::RectangleShape rectangles[mapWidth][mapHeight];	
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+		int rectangleWidth = window.getSize().x/mapWidth;
+		int rectangleHeight = window.getSize().y/mapHeight;
+
+        window.clear();
+		for(int i=0;i<mapWidth;i++)
+			for(int j=0;j<mapHeight;j++)
+			{
+				rectangles[i][j].setSize(sf::Vector2f(rectangleWidth,rectangleHeight));
+				rectangles[i][j].setPosition(sf::Vector2f(i*rectangleWidth,j*rectangleHeight));
+				if(gameMap[i][j]==0)
+				{
+					rectangles[i][j].setFillColor(sf::Color(0,255,0));
+				}
+				else if(gameMap[i][j]==1)
+				{
+					rectangles[i][j].setFillColor(sf::Color(255,0,0));
+				}
+				window.draw(rectangles[i][j]);
+			}
+        window.display();
+    }
+}
+
 void clientRead(int sd)
 {
     ssize_t bufsize = 255, received;
     char buffer[bufsize];
+	cout<<"Oczekiwanie na pozostałych graczy\n";
     while(strcmp(buffer,"start")!=0)
 	    readData(sd, buffer, bufsize);
     cout<<"start gry\n";
+	thread window = thread(sfmlWindow);
     while(1)
     {
         received = readData(sd, buffer, bufsize);
@@ -41,16 +82,18 @@ void clientRead(int sd)
 
 void clientWrite(int sd)
 {
-    while(1)
-    {
         ssize_t bufsize = 255, received;
         char buffer[bufsize];
         received = readData(0, buffer, bufsize);
         writeData(sd, buffer, received);
-    }
 }
 
-
+void initGameMap()
+{
+    for(int i=0; i<mapHeight; i++)
+        for(int j=0; j<mapWidth; j++)
+            gameMap[i][j]=0;
+}
 
 int main() {
     int sd = socket(AF_INET,SOCK_STREAM,0);
@@ -59,15 +102,16 @@ int main() {
     saddr.sin_port = htons(2500);
     saddr.sin_addr.s_addr = inet_addr(ip);
 
+	initGameMap();
     thread read;
     thread write;
     int cd = connect(sd,(sockaddr*) &saddr,sizeof(saddr));
     if(cd==0)
     {
         read = thread(clientRead,sd);
-        write = thread(clientWrite,sd);
+//        write = thread(clientWrite,sd);
         read.join();
-        write.join();
+//        write.join();
     }
 
     return 0;
